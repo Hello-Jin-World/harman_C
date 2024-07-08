76cfe5c9654fa3759f3700b9ddf573e7c2220d60
daniel455 <daniel45598190@gmail.com>
2024-07-08 11:58:49 +0900

    333

--- test.c
+++ test.c
@@ -1,10 +1,153 @@
 #if 1
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <string.h> // string 계산 함수가 들어있음
+#include <string.h>
 #endif
 
+/*문자열 1개로 합치기
+첫번째 문자열 : abcdefg
+두번째 문자열 : ABCDEFGH
+합친 결과 : abcdefgABCDEFG
+*/
+#if 0
+merge_func(char* p1, char* p2, char* po);
+
+int main(void)
+{
+	char input1[80];
+	char input2[80];
+	char output[200];
+
+	printf("첫번째 문자열 : ");
+	fgets(input1, 80, stdin); // 끝에 "\n\0"이 붙는다.
+
+	printf("두번째 문자열 : ");
+	fgets(input2, 80, stdin);
+
+	merge_func(input1, input2, output);
+	printf("합친 결과 : %s\n", output);
+
+	return 0;
+}
+//call by value : 변수를 넘겨주는 방식
+//call by reference : 주소를 넘겨주는 방식
+merge_func(char* p1, char* p2, char* po)
+{
+	char* potmp = po;
+	char* p2tmp = p2;
+
+	strcpy(potmp, p1); //NULL을 만날 대 까지 복사.
+	potmp[strlen(po) - 1] = '\0'; //\n 제거
+
+	while (*potmp != '\0') // potmp를 null이 들어 있는 번지로 이동한다.
+		potmp++;
+
+	while (*p2tmp != '\0')
+	{
+		*potmp++ = *p2tmp++; // 복사가 됨과 동시에 증가가됨	
+	}
+	*potmp = '\0';
+
+	/*printf("%d %d %d", strlen(p1), strlen(p2), strlen(po));
+	for (int j = 0; j < strlen(p1)-1; j++)
+	{
+		*(po + j) = *(p1 + j);
+	}
+	printf("%d", strlen(po));
+	for (int i = strlen(po)-1; i < strlen(po) + strlen(p2) - 2; i++)
+	{
+		*(po + i) = *(p2 + i);
+	}
+	*po = '\0';*/
+}
+#endif
+
+//함수 포인터
+/*
+선택(0: +, 1: -, 2: *, 3: /, 9: exit) : 
+두 수를 입력 (num1, num2)
+결과: num1 + num2 =
+*/
 #if 1
+void add(int a, int b);
+void sub(int a, int b);
+void mul(int a, int b);
+void div(int a, int b);
+int space_check(char* pi);
+
+int main(void)
+{
+	char input[100];
+	int num1, num2;
+	int sp = 0, sel = 0;
+
+	while (1)
+	{
+		printf("선택(0: +, 1: -, 2: *, 3: /, 9: exit) : ");
+		fgets(input, 100, stdin);
+		sel = atoi(input); // 1\n\0  atoi 함수 특징: \n or \0를 만나면 자동으로 멈춤, 1+1일때 +를 만나면 자동으로 멈춤
+		if (sel == 9) break;
+
+		printf("두 수를 입력(num1 num2) : ");
+		fgets(input, 100, stdin); // 111 11
+		sp = space_check(input);
+
+		num1 = atoi(input);
+		num2 = atoi(input + sp + 1);
+
+		switch (sel)
+		{
+		case 0:
+			add(num1, num2);
+			break;
+		case 1:
+			sub(num1, num2);
+			break;
+		case 2:
+			mul(num1, num2);
+			break;
+		case 3:
+			div(num1, num2);
+			break;
+		default:
+			break;
+		}
+	}
+	return 0;
+}
+// call by
+int space_check(char *pi)
+{
+	for (int i = 0; i < 100; i++)
+	{
+		if (*(pi + i) == ' ')
+			return i;
+	}
+}
+// call by value
+void add(int a, int b)
+{
+	printf("결과 : %d + %d = %d\n", a, b, a + b);
+}
+
+void sub(int a, int b)
+{
+	printf("결과 : %d - %d = %d\n", a, b, a - b);
+}
+
+void mul(int a, int b)
+{
+	printf("결과 : %d * %d = %d\n", a, b, a * b);
+}
+
+void div(int a, int b)
+{
+	printf("결과 : %d / %d = %d\n", a, b, a / b);
+}
+#endif
+
+//abc shift 포인터
+#if 0
 void shift_alpha();
 int main(void)
 {
@@ -15,17 +158,18 @@ int main(void)
 	return 0;
 }
 
-void shift_alpha(char* alpha)
+void shift_alpha(char* beta)
 {
-	int j;
-	for (j = 0; j < 25; j++)
+	char temp;
+	for (int i = 0; i < 27; i++)
 	{
-		for (int i = 0; i < 27; i++)
+		printf("%s\n", beta);
+		temp = beta[0];
+		for (int j = 0; j < 25; j++)
 		{
-			printf("%c", *(alpha + i + j));
+			beta[j] = beta[j + 1];
 		}
-		printf("%c", *(alpha + j));
-		printf("\n");
+		beta[25] = temp;
 	}
 }
 #endif
@@ -50,8 +194,8 @@ void upper2lower();
 int main(void)
 {
 	char input[100];	// stack에 메모리가 잡힌다.
-						// 지역변수로 프로그램 모듈이 돌고있는 동안에만 활성화 된다.
-						// 함수 실행이 끝나면 메모리가 삭제 된다.
+	// 지역변수로 프로그램 모듈이 돌고있는 동안에만 활성화 된다.
+	// 함수 실행이 끝나면 메모리가 삭제 된다.
 	while (1)
 	{
 		printf("임의의 문자열을 입력(exit): ");
@@ -68,7 +212,7 @@ int main(void)
 // 변수 이름 앞에 "*"가 들어가면 주소를 저장하는 변수이다.
 // char *p: p라는 변수는 char type의 주소를 저장하는 변수
 //			주소를 넘겨 준다고 해서 call by reference라고 한다.
-void upper2lower(char *p)
+void upper2lower(char* p)
 {
 	printf("p의 주소 : %p\n", p);
 
@@ -76,7 +220,7 @@ void upper2lower(char *p)
 	{
 		if (*(p + i) >= 'A' && *(p + i) <= 'Z') // p+i : 시작번지 + i
 			*(p + i) += 0x20;	// 선언시의 모양과 실행의 모양이 같지만
-								// *p : p의 내용을 번지로 해서 찾아 가는 것이다.
+		// *p : p의 내용을 번지로 해서 찾아 가는 것이다.
 	}
 }
 
@@ -116,9 +260,9 @@ int main(void)
 	return 0;
 }
 
-void cal(int *pa, int *pb, int *ptotal, double *pavg)
+void cal(int* pa, int* pb, int* ptotal, double* pavg)
 {
-	*ptotal =  *pa + *pb;
+	*ptotal = *pa + *pb;
 	*pavg = *ptotal / 2.0;
 }
 #endif
@@ -146,16 +290,16 @@ int main(void)
 
 
 /*************		// 블럭단위 주석 (프로그램 이름, 버전, 작성자, 작성일, 기능 등등 작성)
-* 
-* 
-* 
-**************/  
+*
+*
+*
+**************/
 
 #if 0 // 0이면 실행 안 됨,  1이면 실행
-int main(void){ // void는 가독성때문. 없어도 됨.
+int main(void) { // void는 가독성때문. 없어도 됨.
 	// 변수 (variable) : 자료(data)를 저장하는 메모리 공간
 	char c; // 1byte(8bit) 저장공간, 문자 아님
-			// 저장 범위 : -127 ~ 127
+	// 저장 범위 : -127 ~ 127
 
 	unsigned char uc = 0xff; // 0x00 ~ 0xff, 0 ~ 255
 	// unsigned char uc = 0b11111111;
@@ -179,7 +323,7 @@ int main(void){ // void는 가독성때문. 없어도 됨.
 	//2진수는 프로그램을 따로 찍어야함,
 
 	uc++; // uc = uc + 1;
-		  // overflow 발생
+	// overflow 발생
 
 	printf("uc dec : %d\n", uc); // 0
 	printf("uc hex : %0x\n", uc); // 0
@@ -189,14 +333,14 @@ int main(void){ // void는 가독성때문. 없어도 됨.
 	// MCU를 처음 접했을 때 유용한 함수이다.
 	char arry[10];
 	int iarry[2];
-	printf("char의 size : % d\n",sizeof(char)); // 변수 type
+	printf("char의 size : % d\n", sizeof(char)); // 변수 type
 	printf("c char의 size : % d\n", sizeof(c)); // 변수 이름
 	printf("arry의 size : % d\n", sizeof(arry)); // 10byte
 	printf("iarry의 size : % d\n", sizeof(iarry)); // 8byte
 
 	printf("short : %d\n", sizeof(short));
 	printf("s short : %d\n", sizeof(s));
-	
+
 	printf("int : %d\n", sizeof(int));
 	printf("long : %d\n", sizeof(long));
 	printf("float : %d\n", sizeof(float));
@@ -214,7 +358,7 @@ int main(void){ // void는 가독성때문. 없어도 됨.
 
 	printf("A : %c, size : %d\n", 'A', sizeof((char)'A')); // 1byte로 잡으러면 (char)추가
 	printf("A : %s, size : %d\n", "A", sizeof("A")); // A\0 끝에 null(\0)이 들어간다.
-							 // compiler가 끝에 null을 넣어준다.
+	// compiler가 끝에 null을 넣어준다.
 
 	char a[10] = "abcdefg"; // 실제론abcdefg\0
 	printf("a : %s\n", a); // %s는 null을 만날 때까지 출력 하는 것
@@ -265,7 +409,7 @@ int main(void) {
 	char buff[10]; //지역 변수
 	int num1, num2, result;
 
-	while(1){
+	while (1) {
 		printf("사칙연산(1+1=) (종료 : q)");
 		fgets(buff, 10, stdin);
 		//stdin부터 문자를 입력 받아 buff에 저장한다.
@@ -358,7 +502,7 @@ int main(void)
 	char alpha[10];
 
 	while (1)
-	{	
+	{
 		printf("임의의 문자열을 입력 (종료:quit) : ");
 		fgets(alpha, 100, stdin);
 		if (strncmp(alpha, "quit", 4) == NULL) // strcmp(buff, "quit") == 0
@@ -367,7 +511,7 @@ int main(void)
 		}
 		int len = strlen(alpha);
 
-		for (int i = 0; i < len-1; i++)
+		for (int i = 0; i < len - 1; i++)
 		{
 			// if (alpha[i] >= 'A' && alpha[i] <='Z')
 			if (alpha[i] < 0x5B) // 범위 : A~Z
@@ -380,7 +524,7 @@ int main(void)
 				alpha[i] = alpha[i] - 0x20;
 			}
 		}
-		printf("%s\n",alpha);
+		printf("%s\n", alpha);
 	}
 	return 0;
 }
@@ -397,7 +541,7 @@ int main(void)
 int main(void)
 {
 	printf("%30s\n", "ASCII CODE TABLE"); // 30칸을 오른쪽부터 출력하고 나머지는 공백
-										// %-30s 30칸을 왼쪽부터 출력하고 나머지는 공백
+	// %-30s 30칸을 왼쪽부터 출력하고 나머지는 공백
 	printf("%30s\n\n", "================");
 	printf("%10s%10s%10s%10s\n", "DEC", "HEX", "OCT", "CHAR");
 	printf("%10s%10s%10s%10s\n\n", "===", "===", "===", "====");
@@ -450,7 +594,7 @@ int main(void)
 	}
 	for (int i = 0; i < 128; i++) // i <= 127 보다는 i < 128 이 속도가 더 빠르다
 	{
-		
+
 		printf("%10d%10.2x%10o  ", i, i, i);
 		bin_print(i);
 		printf("%10c\n", i);
@@ -488,16 +632,16 @@ int main(void)
 		bin_print(i);
 		printf("%10s", text[i]);
 
-		printf("%10d%10.2x%10o  ", i+32, i+32, i+32);
+		printf("%10d%10.2x%10o  ", i + 32, i + 32, i + 32);
 		bin_print(i + 32);
 		printf("%10c", i + 32);
 
-		printf("%10d%10.2x%10o  ", i+64, i+64, i+64);
-		bin_print(i+64);
+		printf("%10d%10.2x%10o  ", i + 64, i + 64, i + 64);
+		bin_print(i + 64);
 		printf("%10c", i + 64);
 
-		printf("%10d%10.2x%10o  ", i+96, i+96, i+96);
-		bin_print(i+96);
+		printf("%10d%10.2x%10o  ", i + 96, i + 96, i + 96);
+		bin_print(i + 96);
 		printf("%10c", i + 96);
 
 		printf("\n");
@@ -579,7 +723,7 @@ void bin_div(unsigned char x)
 		x = x / 2;
 		buff[i] = b;
 		if (x == 1) {
-			buff[i+1] = x;
+			buff[i + 1] = x;
 		}
 	}
 	for (int j = 7; j >= 0; j--) {
