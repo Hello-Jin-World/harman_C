ec85db16219452095ddaf02048bda5f9392008da
seongjin <daniel45598190@gmail.com>
2024-07-14 00:08:58 +0900

    ledlaed

--- double_linked.c
+++ double_linked.c
@@ -1,29 +1,67 @@
-#if 0
-#define _CRT_SECURE_NO_WARNINGS
+#if 1
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 
-typedef struct __node
+struct NODE //구조체 선언
 {
-	int value; // ���� ����� ��
-	struct __node* next; // ���� ���
-	struct __node* prev; // ���� ���
-} Node;
+    struct NODE* prev; // 이전 노드를 가리킴
+    int data;
+    struct NODE* next; // 다음 노드를 기리킴
+};
+struct NODE* head; // 첫 번째 노드를 head라고 함
+struct NODE* tail; // 마지막 노드를 tail이라고 함
 
-Node* getNode(Node* ptr)
-{
-	if (ptr == NULL) // �����Ͱ� NULL�� ���� ��� ����
-	{
-		ptr = malloc(sizeof(Node)); // ��� ������ ��ŭ ����
-		ptr->value = 0; // 4byte
-		ptr->next = NULL; // 8byte
-		ptr->prev = NULL; // 8byte
-		printf("node created..\n");
-		return ptr;
-	}
-	printf("node creation failed..");
+//노드 생성 함수
+struct NODE* makenode(int value){
+    struct NODE *node = (struct NODE*)malloc(sizeof(struct NODE)); // 동적 할당
+    node->prev = NULL;
+    node->data = value;
+    node->next = NULL;
+    return node;
 }
 
-void appendFirst(Node* head, int data)
+//출력 함수
+void print(){
+    struct NODE *p;
+    p = head;
+    while(p->next != tail){
+        printf("%d-->",p->data);
+        p = p->next;
+    }
+    printf("%d",p->data);
+}
+
+//초기화 함수
+void init(){
+    head = (struct NODE*)malloc(sizeof(struct NODE));
+    tail = (struct NODE*)malloc(sizeof(struct NODE));
+    head->data = 0;
+    tail->data = 0;
+
+    head->next = tail;
+    head->prev = head;
+    tail->next = tail;
+    tail->prev = head;
+}
+
+//뒤로부터 노드 추가하는 함수
+void push_back(int value){ 
+    struct NODE* newnode = makenode(value);
+    struct NODE* p;
+    p = tail;
+    p->prev->next = newnode;
+    newnode->prev = p->prev;
+    p->prev = newnode;
+    newnode->next = p;
+}
+
+//main함수
+int main(){
+    init(); //head와 tail 초기화 (data = 0)
+    push_back(10); //10 추가
+    push_back(30); //30 추가
+    push_back(50); //50 추가
+    print();  //출력
+    return 0;
+}
 #endif
